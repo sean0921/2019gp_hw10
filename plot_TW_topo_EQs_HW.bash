ffb948d0d5e55511a120f001b2ca8c60741c30c3
sean0921 <sean.li.shin.ho@gmail.com>
2019-05-21 14:05:37 +0800

    implement 將規模大於5的地震用星號表示(-Sa)，大小為規模/8

--- plot_TW_topo_EQs_HW.bash
+++ plot_TW_topo_EQs_HW.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ps=TW_topo_EQs.ps
+ps=TW_topo_EQs_HW.ps
 cpt=topo.cpt
 cpt2=EQ.cpt
 catalog=CWB_2016_2018_M2.catalog
@@ -14,5 +14,6 @@ gmt pscoast -R -JM -Df -W1 -K -O >> $ps
 gmt psscale -C$cpt -Dx14/0+w9/.5+e -Ba500+l"Elevation (m)" -O -K >> $ps
 # plot Earthquakes
 gmt makecpt -Cseis -T0/300/1 > $cpt2
-awk '{print $8,$9,$10,$11/15}' CWB_2016_2018_M2.catalog | gmt psxy -R -JM -C$cpt2 -Sc -O >> $ps
+awk '{if ($11<=5) print $8,$9,$10,$11/20}' CWB_2016_2018_M2.catalog | gmt psxy -R -JM -C$cpt2 -Sc -K -O >> $ps
+awk '{if ($11>5)  print $8,$9,$10,$11/8}'  CWB_2016_2018_M2.catalog | gmt psxy -R -JM -C$cpt2 -Sa -O >> $ps
 
